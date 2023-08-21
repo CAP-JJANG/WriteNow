4088581fbeef562a49f09783c0fab005460cf129
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-08-21 15:10:00 +0900

    FEAT/Update MainActivity - Create Nav

--- app/src/main/java/com/writenow/ui/MainActivity.kt
+++ app/src/main/java/com/writenow/ui/MainActivity.kt
@@ -2,13 +2,25 @@ package com.writenow.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.writenow.R
+import com.writenow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(){
+    private lateinit var navController: NavController
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
 
     }
 }
