e497d3f883e2a0c83a048dfc85a2adfa478c6d9a
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-08-21 18:10:00 +0900

    FEAT/Create Text Copy

--- app/src/main/java/com/writenow/ui/ActionTextDialog.kt
+++ app/src/main/java/com/writenow/ui/ActionTextDialog.kt
@@ -1,14 +1,44 @@
 package com.writenow.ui
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.provider.ContactsContract
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.widget.Toast
 import com.writenow.R
 import com.writenow.base.BaseDialogFragment
 import com.writenow.databinding.DialogActionTextBinding
 
 class ActionTextDialog(txt:String): BaseDialogFragment<DialogActionTextBinding>(R.layout.dialog_action_text) {
+    private val text:String = txt
+
+    override fun initAfterBinding() {
+        super.initAfterBinding()
+
+        binding.tvResultText.text = text
+
+        binding.btnEsc.setOnClickListener{
+            dismiss()
+        }
+        binding.btnCopyText.setOnClickListener{
+            val clipboardManager: ClipboardManager =
+                requireContext().getSystemService(
+                    Context.CLIPBOARD_SERVICE
+                ) as ClipboardManager
+            val clipData = ClipData.newPlainText("text", text)
+            clipboardManager.setPrimaryClip(clipData)
+            Toast.makeText(
+                context,
+                getString(R.string.copied),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
 
     override fun onResume() {
         super.onResume()
