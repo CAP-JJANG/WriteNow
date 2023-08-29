fb6cc3c5a44277fc03f89645fb2779ad7bf889c5
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-08-29 19:30:00 +0900

    FEAT/Create Pop up SOS Dialog

--- app/src/main/java/com/writenow/ui/ShowResultFragment.kt
+++ app/src/main/java/com/writenow/ui/ShowResultFragment.kt
@@ -1,4 +1,27 @@
 package com.writenow.ui
 
-class ShowResultFragment {
+import com.writenow.R
+import com.writenow.base.BaseFragment
+import com.writenow.databinding.FragmentShowResultBinding
+
+class ShowResultFragment : BaseFragment<FragmentShowResultBinding>(R.layout.fragment_show_result) {
+    private lateinit var recordResult:String
+    private lateinit var cmdResult:String
+    private var text:String = ""
+
+    override fun initAfterBinding() {
+        super.initAfterBinding()
+
+        binding.btnAgainRecord.setOnClickListener{
+            navController.navigate(R.id.action_showResultFragment_to_recordFragment)
+        }
+
+        binding.btnDoAction.setOnClickListener{
+            //ActionTextDialog().show(parentFragmentManager, "actionText")
+            if (recordResult=="sos")
+                ActionCmdDialog(recordResult).show(parentFragmentManager, "actionCmd")
+            else
+                ActionTextDialog(recordResult).show(parentFragmentManager, "actionText")
+        }
+    }
 }
